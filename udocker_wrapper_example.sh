c89182109cf21d09a3bd3e8544906f643805673a
Dr. Xiaozhi <jjtseng93@gmail.com>
2026-03-02 02:44:42 +0800

    Update wrapper example script

--- udocker_wrapper_example.sh
+++ udocker_wrapper_example.sh
@@ -1,24 +1,46 @@
 #!/bin/sh
 
 alias udocker='sh "$shr" node "$PKG_RDIR"/js-udocker/udocker.js'
+alias proot-udocker='sh "$shr" proot -l -S / --bind="$PKG_RDIR/bin/mksh":/system/bin/sh --bind="$PKG_RDIR/bin/toybox":/system/bin/toybox /bin/sh "$shr" node "$PKG_RDIR"/js-udocker/udocker.js'
 
 udroot="$HOME"/.udocker/containers
+
+export LD_PRELOAD="" 
+
+
+ensure_dns() {
+
+  if ! [ -s "$rootfs"/etc/resolv.conf ] ; then
+    printf "nameserver 1.1.1.1\nnameserver 8.8.8.8\n">"$rootfs"/etc/resolv.conf
+  fi
+  
+  if ! [ -s "$rootfs"/tmp/resolv.conf ] ; then
+    printf "nameserver 1.1.1.1\nnameserver 8.8.8.8\n">"$rootfs"/tmp/resolv.conf
+  fi
+  
+}
+
 
 if [ "$1" = "run" ] ; then
   shift 1
 
-  if printf -- "$@" | grep -q '\--name=' ; then
-    cname=$(printf -- "$@" | grep -oE '\--name=[^ ]+' | cut -c8-)
-  fi
-
-  while printf -- "$1" | grep -q '^-' 
+  while printf "%s" "$1" | grep -q '^-' 
   do
+
+    if printf "%s" "$1" | grep -q '\--name=' ; then
+      cname=$(printf "%s" "$1" | grep -oE '\--name=[^ ]+' | cut -c8-)
+    elif [ "$1" = "--proot" ] ; then
+      JS_UDOCKER_FORCE_PROOT=1
+    fi
+
     shift 1
+    
   done
 
  if [ -d "$udroot/$1" ] ; then
    cid="$1"
- else
+ else # no existing container
+ 
   # has proot/$1
   if ! [ -z "$1" ] &&
      [ -d "$PKG_RDIR/proot/$1" ] ; then
@@ -30,43 +52,81 @@ if [ "$1" = "run" ] ; then
 
   if ! [ -z "$hasproot" ] &&
      ! echo $reply | grep -qi n ; then
-    export LD_PRELOAD="" 
 
     rootfs="$PKG_RDIR/proot/$1"
 
-    if ! [ -s "$rootfs"/etc/resolv.conf ] ; then
-      printf "nameserver 1.1.1.1\nnameserver 8.8.8.8">"$rootfs"/etc/resolv.conf
-    fi
+    ensure_dns
 
-    exec sh "$PKG_RDIR"/proot/srpr "$rootfs"
+    shift 1
+    if [ -z "$1" ] ; then
+      exec sh "$PKG_RDIR"/proot/srpr "$rootfs"
+    else
+      exec sh "$PKG_RDIR"/proot/srprc "$rootfs" "$@"
+    fi
     
-  fi
+  fi # end if has proot/$1 and yes
   
-  distro=$(printf -- "$1" | grep -oE '[^@]+' | head -n1)
+  distro=$(printf "%s" "$1" | grep -oE '[^@]+' | head -n1)
   echo Creating container from "$distro" as "$cname" 1>&2
 
   udocker pull "$distro"
 
-
+create_container() {
   if [ -z "$cname" ] ; then
     cid=$(udocker create "$distro")
   else
-    udocker create --name="$cname" "$distro" 
-    cid="$cname"
+    udocker create --name="$cname" "$distro"
+    cid=$cname
+  fi
+}
+
+proot_create_container() {
+  echo "  Using proot, may be slow..." 1>&2
+
+  if [ -z "$cname" ] ; then
+    cid=$(proot-udocker create "$distro")
+  else
+    proot-udocker create --name="$cname" "$distro"
+    cid=$cname
+  fi
+}
+
+  
+  if [ -z "$JS_UDOCKER_FORCE_PROOT" ] ; then
+    create_container
+  else
+    proot_create_container
   fi
 
+  echo "Checking for /usr/bin/env: " 1>&2
+  
+  if ! ls "$udroot/$cid/ROOT/usr/bin/env" 1>&2 &&
+     [ -z "$JS_UDOCKER_FORCE_PROOT" ] ; then
+    
+    echo "*** Failed to create from $distro ***" 1>&2
 
- fi # end if $1 is container name/id
+    udocker rm "$cid"
+      
+    proot_create_container
+  fi
+  
 
-  export LD_PRELOAD="" 
+ fi # end if got cid
+    # either $1 is container name/id or create one
+
 
   rootfs="$udroot/$cid"/ROOT
 
-  if ! [ -s "$rootfs"/etc/resolv.conf ] ; then
-    printf "nameserver 1.1.1.1\nnameserver 8.8.8.8">"$rootfs"/etc/resolv.conf
+  ensure_dns
+
+  # $1 is container name/id or distro name
+  shift 1
+  if [ -z "$1" ] ; then
+    exec sh "$PKG_RDIR"/proot/srpr "$rootfs"
+  else
+    exec sh "$PKG_RDIR"/proot/srprc "$rootfs" "$@"
   fi
 
-  exec sh "$PKG_RDIR"/proot/srpr "$rootfs"
 
 elif [ "$1" = "dir" ] ; then
   shift 1
@@ -81,6 +141,8 @@ elif [ "$1" = "dir" ] ; then
     
     exit
   fi
+
+  export cwd=$(pwd)
   
   cd "$udroot"
   
@@ -100,5 +162,9 @@ elif [ "$1" = "dir" ] ; then
 elif [ "$1" = "search" ] ; then
   sh "$shr" xdg-open "https://hub.docker.com/search?q=$2"
 else
+  if [ "$1" = "rm" ] ; then
+    chmod -R 777 "$udroot/$2/"
+  fi
+  
   udocker "$@"
 fi
